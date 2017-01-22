ebf061fa428db0948b15cf2681a1121f2fc4a6c7
manestay <bryanli.ca@gmail.com>
2017-01-21 19:18:21 -0500

    text functionality

--- send_text.py
+++ send_text.py
@@ -1,14 +1,14 @@
 # TODO: place this code when the danger page renders
 from twilio.rest import TwilioRestClient
+import time
+def send(room):
+    t = time.strftime('%H:%M:%S, %d %b %Y')
 
-room = 'room A'
-time = '4:20 AM'
+    # the following line needs your Twilio Account SID and Auth Token
+    client = TwilioRestClient("ACe7e8d8d1b382c5db537f79dd12e8113c", "3124f9c0ca870aa93916ceceda8674f1")
 
-# the following line needs your Twilio Account SID and Auth Token
-client = TwilioRestClient("ACe7e8d8d1b382c5db537f79dd12e8113c", "3124f9c0ca870aa93916ceceda8674f1")
-
-# change the "from_" number to your Twilio number and the "to" number
-# to the phone number you signed up for Twilio with, or upgrade your
-# account to send SMS to any phone number
-client.messages.create(to="+19493108713", from_="+19495417247",
-                       body="There is a fire in {} at {}. Open the NAME app for the fastest, safest escape route.".format(room, time))
+    # change the "from_" number to your Twilio number and the "to" number
+    # to the phone number you signed up for Twilio with, or upgrade your
+    # account to send SMS to any phone number
+    client.messages.create(to="+19493108713", from_="+19495417247",
+                           body="There is a fire in {} at {}. Open the Supah Hot Fire Watcher for the fastest, safest escape route.".format(room, t))
--- simScript.py
+++ simScript.py
@@ -2,13 +2,15 @@ from sort_by_date import getfiles
 import time
 from clarifai.rest import Image as ClImage
 from clarifai.rest import ClarifaiApp
+from send_text import send
+
 appClarifai = ClarifaiApp("gxZl82Rccj9BDbO1py-zze9x2yXllO4K8wgtDfUv", "71dgic1AtUlwc8QX7nm6ppJojeJWqTi7pZ1K_6xy")
 THRESHOLD = .7
 fire_keywords = set(["flame", "fire", "smoke", "heat", "explosion"])
 person_keywords = set(["adult","boy", "man", "woman"])
 person_keywords = set(["adult","boy", "man"])
 FOLDERS = ["pictures/raymond", "pictures/treefire", "pictures/chilling"]
-
+text_sent = False
 fires, people = 0, 0
 
 # get the general model
@@ -17,19 +19,27 @@ model = appClarifai.models.get("general-v1.3")
 #response = model.predict_by_url(url='https://samples.clarifai.com/metro-north.jpg')
 
 def update_file(fires, people):
+    global text_sent
     text_file = open("static/testthis.txt", "w")
     if fires == 0: # case 1
         output = 1
     elif fires == 1 and people >= 2: # case 2
         output = 2
+        room = 'McBain 819'
     elif fires == 2 and people >= 2:
         output = 3
+        room = 'McBain 819 and 822'
     elif fires == 2 and people == 1:
         output = 4
+        room = 'McBain 819 and 822'
     else:
         output = -1
+        room = 'McBain 819'
         #print ("error!!!")
-    print("STATUS: ", output)
+    if output != 1 and not text_sent:
+        send(room)
+        text_sent = True
+    print("STATUS: ", output),
     text_file.write("%s" % output)
     text_file.close()
 
